01a0b3e7ef92a5d15d57cb507b45d8ec696a4ce6
Deepak Sahu <sdeepaksahu1@gmail.com>
2026-07-01 15:27:35 +0530

    Fews updates

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
 
           {/* Resume button */}
           <a
-            href="https://drive.google.com/file/d/12qJDSWnqXGl_zZpl-KS-1CZAdWUN51i5/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1d7WL1tulzHVpMlcHwmotC0GJAgeEUUxb/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
