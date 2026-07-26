789767f957058dca43d4ab5be91d604ae493abb3
Deepak <sdeepaksahu1@gmail.com>
2026-07-26 10:37:39 +0530

    Resume update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -53,7 +53,7 @@ const About = () => {
 
           {/* Resume button */}
           <a
-            href="https://drive.google.com/file/d/1d7WL1tulzHVpMlcHwmotC0GJAgeEUUxb/view"
+            href="https://drive.google.com/file/d/19sHNZ6_VzKHGesDa1xg1J016KeZBvS2Z/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
